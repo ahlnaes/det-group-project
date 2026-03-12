689c33f37462883707aa732332c9ce814f20a9a4
Chuck Long Ching <131591076+NeonstarChuck@users.noreply.github.com>
2026-03-12 11:38:24 +0100

    Hearts is added

--- unity/Assets/Scripts/ButtonManager.cs
+++ unity/Assets/Scripts/ButtonManager.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject[] objects; // Drag Object1–Object6 here in Inspector
+    public GameObject[] objects; // Objects to SHOW
+    public GameObject[] buttons; // Buttons to HIDE (match the order of 'objects')
 
-    public void ShowObject(int index)
+    public void ShowObjectAndHideButton(int index)
     {
+        // 1. Show the object at this index
         if (index >= 0 && index < objects.Length)
         {
             objects[index].SetActive(true);
+        }
+
+        // 2. Hide the button at this same index
+        if (index >= 0 && index < buttons.Length)
+        {
+            buttons[index].SetActive(false);
         }
     }
 }
